d76d6d47164ec9c4659f4216a78d05b2cf5f91b0
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-24 23:13:14 +0900

    fix: static 경로 다시 수정

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -139,9 +139,9 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "myapp", "static")]
 
-STATIC_URL = '/myapp/static/'
+STATIC_URL = 'static/'
 
-MEDIA_URL = '/popup_store_images/'
+MEDIA_URL = 'popup_store_images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
