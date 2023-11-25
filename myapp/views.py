ff4bb365950ced3193bd6bfca4fd4ed62dfde7ac
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-25 15:52:53 +0900

    fix: url로 값 보내기 수정

--- myapp/views.py
+++ myapp/views.py
@@ -9,14 +9,20 @@ def survey(request):
     if request.method == 'GET':
         return render(request, 'survey.html')
     if request.method == 'POST':
-        data = json.loads(request.body)
-        print(data)
-        usermbti = data.get('userMbti')
-        print(usermbti)
+        # data = json.loads(request.body)
+        # print('aaaaa')
+        # print(data)
+        # usermbti = data.get('result')
+        print('bbbbbbbb')
+        # print(usermbti)
+        usermbti = 'ESTJ'
         return redirect('result', usermbti)
+        # return usermbti
 
 def result(request, usermbti):
     if request.method == 'GET':
+        print(usermbti)
         popup = Popup.objects.get(mbti=usermbti)
+        print("popup: ")
         print(popup)
-        return render(request, 'myapp/result.html', {'popup':popup})
+        return render(request, 'result.html', {'popup':popup})
